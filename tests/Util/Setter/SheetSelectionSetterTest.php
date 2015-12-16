9a5c37535cd0e594c3a52a9d13b0eb196a7be50d
77web <info@77-web.com>
2015-12-16 21:33:17 +0900

    [Setter] fixed SheetSelectionSetter to set also tabSelected attribute to sheet xmls

--- tests/Util/Setter/SheetSelectionSetterTest.php
+++ tests/Util/Setter/SheetSelectionSetterTest.php
@@ -4,6 +4,7 @@
 namespace Nanaweb\ExcelSelectionSetter\Util\Setter;
 
 use Nanaweb\ExcelUtil\ZipArchive;
+use Nanaweb\ExcelUtil\Book as BookUtil;
 
 class SheetSelectionSetterTest extends \PHPUnit_Framework_TestCase
 {
@@ -13,22 +14,38 @@ class SheetSelectionSetterTest extends \PHPUnit_Framework_TestCase
      */
     public function test($xmlFixtureFile)
     {
-        $xml = file_get_contents(__DIR__.'/../../data/xml/'.$xmlFixtureFile);
+        $bookXml = file_get_contents(__DIR__.'/../../data/xml/'.$xmlFixtureFile);
+        $sheetXml = file_get_contents(__DIR__.'/../../data/xml/worksheet.xml');
 
         $zip = $this->getMockBuilder(ZipArchive::class)->disableOriginalConstructor()->getMock();
-        $zip->expects($this->atLeastOnce())
-            ->method('getFromName')
-            ->with('xl/workbook.xml')
-            ->will($this->returnValue($xml))
+        $bookUtil = $this->getMock(BookUtil::class);
+
+        $bookUtil->expects($this->atLeastOnce())
+            ->method('makeSheetMap')
+            ->with($zip)
+            ->will($this->returnValue([1 => 'シート1', 2 => 'シート2']))
         ;
-        $zip->expects($this->once())
+        $bookUtil->expects($this->once())
+            ->method('makeSheetFileMap')
+            ->with($zip)
+            ->will($this->returnValue(['シート1' => 'xl/sheets/sheet1.xml', 'シート2' => 'xl/sheets/sheet2.xml']))
+        ;
+        
+        $zip->expects($this->exactly(3))
+            ->method('getFromName')
+            ->with($this->logicalOr('xl/workbook.xml', 'xl/sheets/sheet1.xml', 'xl/sheets/sheet2.xml'))
+            ->will($this->returnCallback(function($arg) use ($bookXml, $sheetXml){
+                return $arg == 'xl/workbook.xml' ? $bookXml : $sheetXml;
+            }))
+        ;
+        $zip->expects($this->exactly(3))
             ->method('addFromString')
-            ->with('xl/workbook.xml', $this->callback(function($xml){
-                return strpos($xml, 'activeTab="1"') !== false;
+            ->with($this->logicalOr('xl/workbook.xml', 'xl/sheets/sheet1.xml', 'xl/sheets/sheet2.xml'), $this->callback(function($xml){
+                return strpos($xml, 'activeTab="1"') !== false || strpos($xml, 'tabSelected="1"') !== false || strpos($xml, 'tabSelected="0"') !== false;
             }))
         ;
 
-        $setter = new SheetSelectionSetter();
+        $setter = new SheetSelectionSetter($bookUtil);
         $setter->set($zip, 'シート2');
     }
 
